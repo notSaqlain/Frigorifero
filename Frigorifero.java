1424eed0ce5647f4b5dd220ddd59076397c25e92
notSaqlain <iamnotsaqlain@gmail.com>
2024-11-19 22:08:09 +0100

    Add properties and constructor to Frigorifero class

--- Frigorifero.java
+++ Frigorifero.java
@@ -1,3 +1,16 @@
+import java.time.LocalDate;
+
 public class Frigorifero {
-    
+    private int id;
+    private String des;
+    private int cal;
+    private LocalDate expiryDate;
+
+    public Frigorifero(int id, String des, int cal, LocalDate expiryDate) {
+        this.id = id;
+        this.des = des;
+        this.cal = cal;
+        this.expiryDate = expiryDate;
+    }
+
 }
